79ae31abe970ff6560c0f56e44360d62a9cb2fe3
Ivo Beerens <ibeerens@gmail.com>
2024-08-28 09:28:09 +0200

    Add pagefile

---
+++ Scripts/set-pagefile16-32gb.ps1
@@ -0,0 +1,11 @@
+# Set Pagefile
+$pagefile = Get-WmiObject Win32_ComputerSystem -EnableAllPrivileges
+$pagefile.AutomaticManagedPagefile = $false
+$pagefile.put() | Out-Null
+
+$pagefileset = Get-WmiObject Win32_pagefilesetting
+$pagefileset.InitialSize = 16394
+$pagefileset.MaximumSize = 32768
+$pagefileset.Put() | Out-Null
+
+Gwmi win32_Pagefilesetting | Select Name, InitialSize, MaximumSize
